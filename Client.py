c78df99ab5ead6118735524fcdd21483544a872a
It's me Abel <abel97.ag@gmail.com>
2021-10-20 22:21:34 +0200

    Created client & used the dotEnv for env variables

---
+++ Client.py
@@ -0,0 +1,17 @@
+# bot.py
+#This is a client class that represents the connection to discord
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():   #Event handler that is executed when the client has established connection to Discord
+    print(f'{client.user} has connected to Discord!')
+
+client.run(TOKEN)
