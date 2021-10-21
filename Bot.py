e49729a770a555194d0ff937ebe3ed00d8270139
It's me Abel <abel97.ag@gmail.com>
2021-10-21 03:30:38 +0200

    Bot can now access member list and server info

--- Bot.py
+++ Bot.py
@@ -8,11 +8,27 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+SERVER = os.getenv('DISCORD_SERVER')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():   #Event handler that is executed when the client has established connection to Discord
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds: #loops the through the list of servers the bot is connected to and matches it 
+        if guild.name == SERVER:        #--with the one we wanted
+            break
+    
+    
+
+    print(
+        
+        f'{client.user} is connected to the following server:\n'
+        f'{guild.name} id: {guild.id}\n'
+        
+        )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild members: \n {members}')
+
 
 client.run(TOKEN)
